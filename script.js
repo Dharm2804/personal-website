37db6ba09779eb6bd386ea33a6cd185948f26155
Dharm2804 <125880807+Dharm2804@users.noreply.github.com>
2023-12-22 12:46:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var typed=new Typed(".multiple-text",{
+    strings: ["Student" , "Mathematician","Data Scientist"],
+    typespeed:60,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+var typed=new Typed(".multiple-te",{
+    strings: ["DSA : Hello, I am a DSA expert. When it comes to data structures, I excel at processing and manipulating information in a way that enables efficient storage, retrieval, and manipulation of data. Whether it's arrays, linked lists, trees, graphs, or other complex structures, I can navigate through them to help analyze and solve problems. My capabilities extend to various algorithms and methods used in organizing and managing data effectively. If you have questions or tasks related to data structures, feel free to ask, and I'll do my best to assist you!" , 
+    "Maths :  Hello, I am Mathematician , a virtual assistant developed by OpenAI. I am proficient in various mathematical concepts and can assist you with a wide range of mathematical problems and inquiries. Whether you need help with basic arithmetic, algebra, calculus, statistics, or any other mathematical topic, feel free to ask, and I'll do my best to provide clear and accurate explanations.",
+    ],
+    typespeed:100000,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+})
